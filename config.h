26d82d04e547367e97cf8536ef6f048807964da8
root <elespink@gmail.com>
2022-06-08 16:08:34 +0200

    Systray patch.

--- config.h
+++ config.h
@@ -1,18 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int snap      = 0;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Jetbrains mono:size=13" };
-static const char dmenufont[]       = "Jetbrains mono:size=14";
-static const char col_gray1[]       = "#0d0d0d";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#2777ff";
-static const char *colors[][3]      = {
+static const unsigned int borderpx          = 4;   /* border pixel of windows */
+static const unsigned int snap              = 0;   /* snap pixel */
+static const unsigned int systraypinning    = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft     = 1;   /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing    = 2;   /* systray spacing */
+static const int systraypinningfailfirst    = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray                = 1;   /* 0 means no systray */
+static const int showbar                    = 1;   /* 0 means no bar */
+static const int topbar                     = 0;   /* 0 means bottom bar */
+static const char *fonts[]                  = { "Jetbrains Mono:size=13" };
+static const char dmenufont[]               = "Jetbrains Mono:size=14";
+static const char col_gray1[]               = "#0d0d0d";
+static const char col_gray2[]               = "#444444";
+static const char col_gray3[]               = "#bbbbbb";
+static const char col_gray4[]               = "#eeeeee";
+static const char col_cyan[]                = "#2777ff";
+static const char *colors[][3]              = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
@@ -21,34 +26,33 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact            = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster            = 1;    /* number of clients in master area */
+static const int resizehints        = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen     = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "][",      tile },    /* first entry is default */
-	{ "><",      NULL },    /* no layout function means floating behavior */
-	{ "{}",      monocle },
+	{ "][",       tile },    /* first entry is default */
+	{ "><",       NULL },    /* no layout function means floating behavior */
+	{ "{M}",      monocle },
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ ControlMask|Mod1Mask,         KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
@@ -62,13 +66,10 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 
-// Xfce like shortcut.
-
-//#include "shiftview.c"
 #include "shift-tools.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ ControlMask,                  XK_Escape,  spawn,          {.v = dmenucmd } },
+	{ ControlMask,                  XK_Escape, spawn,          {.v = dmenucmd } },
 	{ ControlMask|Mod1Mask,         XK_t,      spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ Mod1Mask,                     XK_Tab,    focusstack,     {.i = +1 } },
@@ -78,10 +79,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ ControlMask|Mod1Mask,         XK_Left,   shiftview,      {.i = -1 } },
+    { ControlMask|Mod1Mask,         XK_Left,   shiftview,      {.i = -1 } },
 	{ ControlMask|Mod1Mask,         XK_Right,  shiftview,      {.i = +1 } },
-	{ ControlMask|Mod1Mask,         XK_Home,    shiftboth,      {.i = -1 } },
-	{ ControlMask|Mod1Mask,         XK_End,   shiftboth,      {.i = +1 } },
+	{ ControlMask|Mod1Mask,         XK_Home,    shiftboth,     {.i = -1 } },
+	{ ControlMask|Mod1Mask,         XK_End,   shiftboth,       {.i = +1 } },
 //	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ ControlMask,                  XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -111,8 +112,8 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
@@ -123,3 +124,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
