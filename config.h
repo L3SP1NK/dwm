923689d2619f4144b187977f59bebd66218c6de5
root <elespink@gmail.com>
2022-06-30 21:51:42 +0200

    Minor changes.

--- config.h
+++ config.h
@@ -65,6 +65,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "xterm", NULL };
+//static const char *termcmd[]  = { "xterm_t", NULL }; // tabbed version of xterm
 
 #include "shift-tools.c"
 static Key keys[] = {
@@ -81,8 +82,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
     { ControlMask|Mod1Mask,         XK_Left,   shiftview,      {.i = -1 } },
 	{ ControlMask|Mod1Mask,         XK_Right,  shiftview,      {.i = +1 } },
-	{ ControlMask|Mod1Mask,         XK_Home,    shiftboth,     {.i = -1 } },
-	{ ControlMask|Mod1Mask,         XK_End,   shiftboth,       {.i = +1 } },
+  	{ ControlMask|Mod1Mask,         XK_Home,   shiftboth,     {.i = -1 } },
+	{ ControlMask|Mod1Mask,         XK_End,    shiftboth,       {.i = +1 } },
 //	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ ControlMask,                  XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
